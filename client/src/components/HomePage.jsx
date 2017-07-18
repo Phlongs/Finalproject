b796e0dafab090b278d20c5866c35ad5f5589b50
Phlongs <phlongs@gmail.com>
2017-07-18 13:32:36 -0400

    update bio

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Slider from 'react-slick';
 import SimpleSlider from './SimpleSlider.jsx';
 import Cards from './Cards.jsx';
-import Footer from './Footer.jsx';
+
 
 
 const HomePage = () => (
